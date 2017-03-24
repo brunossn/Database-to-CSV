9b802351e4fd64f4aabcd972b415e63ebde6fb3c
Bruno Soares <brunossn+coding@gmail.com>
2017-03-24 13:29:45 -0300

    Funciona também com bancos em SQLite

--- GeraCSV/Program.cs
+++ GeraCSV/Program.cs
@@ -2,11 +2,19 @@
 using System.Data.SqlClient;
 using System.IO;
 using System.Text;
+using System.Data.SQLite;
+using System.Data.Common;
 
 namespace GeraCSV
 {
     class Program
     {
+        private enum eTipoBanco
+        {
+            SQLServer = 1,
+            Sqlite = 2
+        }
+
         static void Main(string[] args)
         {
             int linhaAtual = 0;
@@ -15,43 +23,56 @@ namespace GeraCSV
             try
             {
                 string mensagemValida = valida(args);
-
+                
                 if (mensagemValida == "")
                 {
-                    string arquivoEntrada = args[0];
-                    string arquivoSaida = args[1];
+                    string arquivoQuery = args[0];
+                    string saidaCSV = args[1];
                     string stringConexao = args[2];
-                    string query = File.ReadAllText(arquivoEntrada);
-                    StringBuilder saida = new StringBuilder();
+                    eTipoBanco tipoBanco = (eTipoBanco)Convert.ToInt32(args[3]);
+                    
+                    var query = File.ReadAllText(arquivoQuery);
+                    var saida = new StringBuilder();
 
                     // Conecta ao SQL
-                    SqlConnection sqlConexao = new SqlConnection(stringConexao);
-                    sqlConexao.Open();
-
-                    SqlCommand sqlComando = new SqlCommand(query, sqlConexao);
-                    sqlComando.CommandTimeout = 90000;
-
-                    SqlDataReader sqlDataReader = sqlComando.ExecuteReader();
-                    
-
-                    // Cria CSV
-                    while (sqlDataReader.Read())
+                    DbConnection conexao;
+                    DbCommand comando;
+                    if (tipoBanco == eTipoBanco.SQLServer)
                     {
-                        linhaAtual++;
-
-                        for (colunaAtual = 0; colunaAtual < sqlDataReader.FieldCount; colunaAtual++)
-                        {
-                            if (colunaAtual != 0) saida.Append(";");
-                            saida.Append(sqlDataReader[colunaAtual].ToString().Trim());
-                        }
-
-                        saida.Append("\n");
+                        conexao = new SqlConnection(stringConexao);
+                        comando = new SqlCommand();
                     }
+                    else
+                    {
+                        conexao = new SQLiteConnection(stringConexao);
+                        comando = new SQLiteCommand();
+                    }
+                    
+                    conexao.Open();
+                    comando.CommandText = query;
+                    comando.Connection = conexao;
+                    comando.CommandTimeout = 90000;
 
+                    using (DbDataReader dr = comando.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            linhaAtual++;
+
+                            for (colunaAtual = 0; colunaAtual < dr.FieldCount; colunaAtual++)
+                            {
+                                if (colunaAtual != 0) saida.Append(";");
+                                saida.Append(dr[colunaAtual].ToString().Trim());
+                            }
+
+                            saida.Append("\n");
+                        }
+                    }
+                    
                     // Saída
-                    File.WriteAllText(arquivoSaida, saida.ToString().Trim(), Encoding.UTF8);
+                    File.WriteAllText(saidaCSV, saida.ToString().Trim(), Encoding.UTF8);
 
-                    sqlConexao.Close();
+                    conexao.Close();
                 }
                 else
                 {
@@ -72,9 +93,11 @@ namespace GeraCSV
         {
             string retorno = "";
 
-            if(args.Length != 3)
+            if(args.Length != 4)
             {
-                retorno = "Execute o sistema passando três parâmetros:\n[1] - Arquivo contendo a query\n[2] - Arquivo CSV que será gerado;\n[3] - String de conexão com o banco de dados";
+                retorno = "Execute o sistema passando quatro parâmetros:\n[1] - Arquivo TXT contendo a query\n" +
+                    "[2] - Caminho do arquivo CSV que será gerado;\n[3] - String de conexão com o banco de dados\n" +
+                    "[4] - Tipo do banco de dados (1 - SQL Server / 2 - SQLite)";
             }
             else
             {
